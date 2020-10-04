569cfd53f624a45c70b455575a085c9dd40691ee
dnsgk4983 <64947285+dnsgk4983@users.noreply.github.com>
2020-10-04 16:32:14 +0900

    s

--- resources/js/script.js
+++ resources/js/script.js
@@ -351,7 +351,7 @@ $(function()
 
 var viewMode = getCookie("view-mode");
 if(viewMode == "desktop"){
-       viewport.setAttribute('content', 'width=1024');
+       viewport.setAttribute('content', 'width=1240');
 }else if (viewMode == "mobile"){
        viewport.setAttribute('content', 'width=device-width,initial-scale=1.0,maximum-scale=1.0,user-scalable=no');
 }
